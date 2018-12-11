cdde375bb3f8f22b59403ff66fa1e7965fc04b93
lejay_c <cedric.lejay@epitech.eu>
2018-12-11 11:48:56 +0100

    Sign up

--- Watermelon/watermelon/src/containers/Signup.js
+++ Watermelon/watermelon/src/containers/Signup.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { HelpBlock, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
 import "./Signup.css";
+var Parse = require('parse').Parse;
 
 export default class Signup extends Component {
 	constructor(props) {
@@ -18,13 +19,15 @@ export default class Signup extends Component {
 	}
 
 	validateForm() {
-		return (this.state.email.length > 0
-			&& this.state.passwd.len$ > 0
-			&& this.state.passwd === this.state.confirmPasswd);
-	}
+    	return (
+      		this.state.email.length > 0 &&
+      		this.state.passwd.length > 0 &&
+      		this.state.passwd === this.state.confirmPasswd
+    	);
+  	} 
 
 	validateConfirmationForm() {
-		return this.state.confirmationCode.length > 0;
+		return (this.state.confirmationCode.length > 0);
 	}
 
 	handleChange = event => {
@@ -33,11 +36,112 @@ export default class Signup extends Component {
 		});
 	}
 
-	handleSubmit = event => {
+	handleSubmit = async event => {
+		event.preventDefault();
+
+    this.setState({ isLoading: true });
+    var user = new Parse.User();
+    user.set("username", "test");
+    user.set("email", this.state.email);
+    user.set("password", this.state.passwd);
+    try {
+      await user.signUp();
+      this.setState({ newUser: user });
+    } catch (error) {
+      alert("Error : " + error.code + " " + error.message);
+    }
+    this.setState({ isLoading: false });
+	}
+
+	handleConfirmationSubmit = async event => {
 		event.preventDefault();
 
 		this.setState({ isLoading: true });
-		this.setState({ newUser: "test" });
-		this.setState({ isLoading: false });
+
+    try {
+      await Parse.User.logIn(this.state.email, this.state.passwd);
+      this.props.userHasAuthenticated(true);
+      this.props.history.push("/");
+    } catch (error) {
+      alert("Error : " + error.code + " " + error.message);
+      this.setState({ isLoading: false });
+    }
+	}
+
+	renderConfirmationForm() {
+	    return (
+	      	<form onSubmit={this.handleConfirmationSubmit}>
+	        	<FormGroup controlId="confirmationCode" bsSize="large">
+	          	<ControlLabel>Confirmation Code</ControlLabel>
+	          	<FormControl
+	            	autoFocus
+	            	type="tel"
+	            	value={this.state.confirmationCode}
+	            	onChange={this.handleChange}
+	          	/>
+	          	<HelpBlock>Please check your email for the code.</HelpBlock>
+	        	</FormGroup>
+	        	<LoaderButton
+	          		block
+	          		bsSize="large"
+	          		disabled={!this.validateConfirmationForm()}
+	          		type="submit"
+	          		isLoading={this.state.isLoading}
+	          		text="Verify"
+	          		loadingText="Verifying…"
+	        	/>
+	      	</form>
+	    );
+	  }
+
+	  renderForm() {
+	    return (
+	      	<form onSubmit={this.handleSubmit}>
+		        <FormGroup controlId="email" bsSize="large">
+		          <ControlLabel>Email</ControlLabel>
+		          <FormControl
+		            autoFocus
+		            type="email"
+		            value={this.state.email}
+		            onChange={this.handleChange}
+		          />
+		        </FormGroup>
+		        <FormGroup controlId="passwd" bsSize="large">
+		          <ControlLabel>Password</ControlLabel>
+		          <FormControl
+		            value={this.state.passwd}
+		            onChange={this.handleChange}
+		            type="password"
+		          />
+		        </FormGroup>
+		        <FormGroup controlId="confirmPasswd" bsSize="large">
+		          <ControlLabel>Confirm Password</ControlLabel>
+		          <FormControl
+		            value={this.state.confirmPasswd}
+		            onChange={this.handleChange}
+		            type="password"
+		          />
+		        </FormGroup>
+		        <LoaderButton
+		          block
+		          bsSize="large"
+		          disabled={!this.validateForm()}
+		          type="submit"
+		          isLoading={this.state.isLoading}
+		          text="Signup"
+		          loadingText="Signing up…"
+		        />
+		    </form>
+		);
+	}
+
+	render() {
+	    return (
+	      <div className="Signup">
+	        {this.state.newUser === null
+	          ? this.renderForm()
+	          : this.renderConfirmationForm()}
+	      </div>
+	    );
 	}
 }
