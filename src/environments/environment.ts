60564c1c9a2e54b3cf9dba0807ae259e64865bfa
Miran <mirann.her@gmail.com>
2019-11-14 01:00:56 +0100

    debut implementation api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  useHash: false,
+  serverUrl: ''
+  // use this line if you want to use a server hosted on your computer
+	// serverUrl: '//localhost:3000',
 };
 
 /*
