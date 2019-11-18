aca51fb037b52329985ce8416fbe4cc1d0937d0d
Lois Antoine <lois.antoine@epitech.eu>
2019-11-18 13:37:00 +0100

    Création item/debut d'affichage des item

--- src/app/components/event/event.component.ts
+++ src/app/components/event/event.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as Parse from 'parse';
 import { ActivatedRoute } from '@angular/router';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-event',
@@ -10,17 +11,29 @@ import { ActivatedRoute } from '@angular/router';
 
 export class EventComponent implements OnInit {
 
+  needsprice = new FormControl('', [Validators.required, Validators.requiredTrue]);
+  needsquant = new FormControl('', [Validators.required, Validators.requiredTrue]);
+  needsname = new FormControl('', [Validators.required, Validators.requiredTrue]);
   constructor(private route: ActivatedRoute) { }
   eventId;
   eventList;
+  itemList;
   event;
   nameEvent;
+  needsEvent;
   queryEvent;
+  queryNeeds;
+  priceNeeds;
+  namesNeeds;
+  quantNeeds;
 
   ngOnInit() {
     let id = this.route.snapshot.paramMap.get('id');
     this.eventId = id;
+    this.itemList = [];
+    this.queryNeeds = new Parse.Query('Needs');
     this.findEvent();
+    console.log('this.itemList :', this.itemList);
   }
 
   async findEvent() {
@@ -36,7 +49,53 @@ export class EventComponent implements OnInit {
       if (this.queryEvent == this.eventId) {
         this.event = item;
         this.nameEvent = this.event.get('eventName');
+        this.needsEvent = this.event.get('itemList');
+        if (!this.needsEvent) {
+          this.needsEvent = [];
+        }
+
+        for (let item of this.needsEvent)
+        {
+          this.queryNeeds.get(item)
+          .then(res => {
+            this.itemList.push(res);
+          }, err => {
+            alert(err);
+          })
+        }
       }
+    }
+  }
+
+  async createNeeds() {
+    const needsnameVal = this.needsname.value as string;
+    const needspriceVal = parseInt(this.needsprice.value as string);
+    const needsquantVal = parseInt(this.needsquant.value as string);
+
+    if (needsnameVal != null && needspriceVal != null && needsquantVal != null) {
+      var needs = Parse.Object.extend('Needs');
+      var newNeed = new needs();
+
+      newNeed.set('Name', needsnameVal);
+      newNeed.set('Price', needspriceVal);
+      newNeed.set('Quantity', needsquantVal);
+
+      newNeed.save()
+      .then(res => {
+        this.needsEvent.push(newNeed.id);
+        this.event.set('itemList', this.needsEvent);
+        this.event.save()
+        .then(res => {
+          console.log('Maj item list');
+        }, err=> {
+          alert(err);
+        })
+
+        alert('Votre objet a été ajouté avec succès!');
+        location.reload();
+      }, err => {
+        alert(err);
+      })
     }
   }
 }
