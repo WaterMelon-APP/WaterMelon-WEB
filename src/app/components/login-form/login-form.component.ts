2236022f7deb34b2937653b3d5e4cebf8196eeca
Loïs Antoine <hemoragys@gmail.com>
2020-06-17 15:31:49 +0200

    login api v0

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -2,8 +2,28 @@ import {Component, OnInit} from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
 import * as Parse from 'parse';
 import {MatDialogRef} from '@angular/material';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 
 import { User } from '@models/user';
+
+export interface LogResponse {
+  Id: string;
+  Name: string;
+  Username: string;
+  Password: string;
+  Email: string;
+  Token: string;
+  FirstName: string;
+  LastName: string;
+  Phone: string;
+  Birthdate: Date;
+  ProfilePicture: string;
+  Events: Array<string>;
+}
 
 @Component({
   selector: 'app-login-form',
@@ -12,6 +32,11 @@ import { User } from '@models/user';
 })
 
 export class LoginFormComponent implements OnInit {
+  private tokenSource = new BehaviorSubject<string>("");
+  private idSource = new BehaviorSubject<string>("");
+  currentToken = this.tokenSource.asObservable();
+  currentId = this.idSource.asObservable();
+
 
   hide = true;
 
@@ -19,11 +44,11 @@ export class LoginFormComponent implements OnInit {
   email = new FormControl('', [Validators.required, Validators.email]);
   password = new FormControl('', [Validators.required, Validators.requiredTrue]);
 
-  constructor(private dialogRef: MatDialogRef<LoginFormComponent>) { }
+  constructor(private dialogRef: MatDialogRef<LoginFormComponent>, private http: HttpClient) { }
 
-getErrorMessage() {
-  
-}
+  getErrorMessage() {
+
+  }
 
   ngOnInit() {
   }
@@ -32,7 +57,26 @@ getErrorMessage() {
     const emailVal = this.email.value as string;
     const passwordVal = this.password.value as string;
     if (emailVal != null || passwordVal != null) {
-      Parse.User.logIn(emailVal, passwordVal)
+      
+      const header: Object = {
+        headers: new HttpHeaders({
+            Accept: 'application/json',
+            'Content-Type':  'application/json'
+        })
+      };
+      const user = '{ "Username": "' + emailVal + '", "Password": "' + passwordVal + '" }';
+      var juser = JSON.parse(user);
+      console.log('juser :>> ', juser);
+      this.http.post<LogResponse>("https://watermelon-api20200526035653.azurewebsites.net/api/users/login", juser, header)
+      .subscribe(logResponse => {
+          this.tokenSource.next(logResponse.Token);
+          this.idSource.next(logResponse.Id);
+        },
+        error => { 
+            alert("Une erreur est survenue");
+        }
+      );
+      /*Parse.User.logIn(emailVal, passwordVal)
         .then(res => {
           this.dialogRef.close();
 
@@ -55,7 +99,7 @@ getErrorMessage() {
 
           }
           alert(msg);
-        });
+        });*/
 
     }
   }
