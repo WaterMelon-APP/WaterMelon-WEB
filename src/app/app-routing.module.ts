f737ab641a13583a3415b9a8e55ed84ff77eb864
Lysa Damon <lysa.damon@epitech.eu>
2020-12-03 15:27:45 +0100

    signin and register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { EventComponent } from './components/event/event.component';
 import { EditEventComponent } from './components/event/edit-event.component';
 import { SearchComponent } from './components/search/search-page.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { RegisterFormComponent } from './components/register/register-form.component';
 
 const routes: Routes = [
   {path: '', component: HomePageComponent},
@@ -14,7 +16,9 @@ const routes: Routes = [
   {path: 'list-user', component: ListEventUserPageComponent},
   {path: 'event/:id', component: EventComponent },
   {path: 'event-edit/:id', component: EditEventComponent},
-  {path: 'search/:id', component: SearchComponent}
+  {path: 'search/:id', component: SearchComponent},
+  {path: 'register', component: RegisterFormComponent},
+  {path: 'login', component: LoginFormComponent}
 ];
 
 @NgModule({
@@ -27,4 +31,6 @@ export const routingComponent = [HomePageComponent,
                                 ListEventUserPageComponent,
                                 EventComponent,
                                 EditEventComponent,
-                                SearchComponent]
+                                SearchComponent,
+                                RegisterFormComponent,
+                                LoginFormComponent]
