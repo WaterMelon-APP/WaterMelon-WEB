0a61e713bb54b02e5534a31c6f877ef431d42247
Lysa Damon <lysa.damon@epitech.eu>
2020-12-10 16:14:55 +0100

    /reset component added + clean useless components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,18 +18,7 @@ import { RegisterFormComponent } from './components/register/register-form.compo
 import { CreateEventComponent } from './components/event/create-event.component';
 import { EditEventComponent } from './components/event/edit-event.component';
 import { SearchComponent } from './components/search/search-page.component';
-
-import { CardEventAdminComponent } from './components/card-event-admin/card-event-admin.component';
-import { CardEventMemberComponent } from './components/card-event-member/card-event-member.component';
-import { CardDiscoveryComponent } from './components/card-discovery/card-discovery.component';
-import { CardEditEventComponent } from './components/card-edit-event/card-edit-event.component';
-import { AlertComponent } from './components/alert/alert.component';
-import { NotifMenuComponent } from './components/notif-menu/notif-menu.component';
-import { NotifInvitMenuComponent } from './components/notif-invit-menu/notif-invit-menu.component';
-import { NotifInfoMenuComponent } from './components/notif-info-menu/notif-info-menu.component';
-import { NotifModifEventComponent } from './components/notif-modif-event/notif-modif-event.component';
-import { NotifInvitResponseComponent } from './components/notif-invit-response/notif-invit-response.component';
-import { DisplayEventComponent } from './components/display-event/display-event.component';
+import { ResetComponent } from './components/reset/reset.component';
 
 import { NotificationsComponent } from './components/notifications/notifications.component';
 
@@ -50,18 +39,7 @@ import { NotificationsComponent } from './components/notifications/notifications
     EventComponent,
     SearchComponent,
     NotificationsComponent,
-
-    AlertComponent,
-    CardDiscoveryComponent,
-    CardEditEventComponent,
-    CardEventAdminComponent,
-    CardEventMemberComponent,
-    DisplayEventComponent,
-    NotifInfoMenuComponent,
-    NotifInvitMenuComponent,
-    NotifInvitResponseComponent,
-    NotifMenuComponent,
-    NotifModifEventComponent
+    ResetComponent,
 
   ],
   imports: [
--- src/app/components/reset/reset.component.spec.ts
+++ src/app/components/reset/reset.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CardEventMemberComponent } from './card-event-member.component';
+import { ResetComponent } from './reset.component';
 
-describe('CardEventMemberComponent', () => {
-  let component: CardEventMemberComponent;
-  let fixture: ComponentFixture<CardEventMemberComponent>;
+describe('ResetComponent', () => {
+  let component: ResetComponent;
+  let fixture: ComponentFixture<ResetComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CardEventMemberComponent ]
+      declarations: [ ResetComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CardEventMemberComponent);
+    fixture = TestBed.createComponent(ResetComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
